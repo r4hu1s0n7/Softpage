a212aebb7d7f1952fbdaa460c8c4d29250ecee17
r4hu1s0n7 <40057302+r4hu1s0n7@users.noreply.github.com>
2021-01-18 15:52:04 +0530

    Create Summarizer.py

---
+++ Summarizer.py
@@ -0,0 +1,26 @@
+try:
+	import json
+	import http.client
+	import requests
+except Exception as e:
+	print("Import error")
+
+
+# summarizer
+def summarizer(load):
+
+	payload = json.dumps({"text":load})
+	conn = http.client.HTTPSConnection("text-monkey-summarizer.p.rapidapi.com")
+	headers = {'content-type': "application/json",'x-rapidapi-key': "YOUR API KEY",'x-rapidapi-host': "text-monkey-summarizer.p.rapidapi.com"}
+	conn.request("POST", "/nlp/summarize", payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	data = json.loads(data)
+	sdata="SUMMARY : \n\n"
+	for i in data['snippets']:
+		sdata+='- '+i+'\n'
+	for i in data['summary'].split('\n'):
+		if len(i)>15:
+			sdata+='- '+i+'\n'
+
+	return sdata
